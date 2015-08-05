eefe41c3dccded195bf85792b06c3aed6481c7b0
sjmp <samueljamespartridge@gmail.com>
2015-08-05 14:12:42 +0100

    Routing, dummy files, login page

--- app/app.routes.js
+++ app/app.routes.js
@@ -5,12 +5,48 @@ angular
     .config(function($routeProvider) {
         $routeProvider
 
-        //story router
-//        .when('/', {
-//            templateUrl: 'app/components/scenepage/sceneView.html',
-//            controller: 'sceneController as ctrl'
-//        });
+        //login
+        .when('/', {
+            templateUrl: 'app/components/login/loginView.html'
+//            ,controller: 'sceneController as ctrl'
+        })
+    
+        //register
+        .when('/register', {
+            templateUrl: 'app/components/register/registerView.html'
+//            ,controller: 'sceneController as ctrl'
+        })
 
+        //home
+        .when('/home', {
+            templateUrl: 'app/components/home/homeView.html'
+//            ,controller: 'sceneController as ctrl'
+        })
+        
+        //symptoms  
+        .when('/symptoms', {
+            templateUrl: 'app/components/symptoms/symptomsView.html'
+//            ,controller: 'sceneController as ctrl'
+        })
+        
+        //history
+        .when('/history', {
+            templateUrl: 'app/components/history/historyView.html'
+//            ,controller: 'sceneController as ctrl'
+        })
+        
+        //weight
+        .when('/weight', {
+            templateUrl: 'app/components/weight/weightView.html'
+//            ,controller: 'sceneController as ctrl'
+        })
+        
+        //dietician
+        .when('/dietician', {
+            templateUrl: 'app/components/dietician/dieticanView.html'
+//            ,controller: 'sceneController as ctrl'
+        });
+        
     });
     
 }());
