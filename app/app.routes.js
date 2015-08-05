61444da5690f73feef60116f75eb5674ae3c8c2f
sjmp <samueljamespartridge@gmail.com>
2015-08-05 12:16:58 +0100

    Initial Structure

---
+++ app/app.routes.js
@@ -0,0 +1,17 @@
+(function() {
+'use strict';
+angular
+    .module('appetiteApp')
+    .config(function($routeProvider) {
+        $routeProvider
+
+        //story router
+//        .when('/', {
+//            templateUrl: 'app/components/scenepage/sceneView.html',
+//            controller: 'sceneController as ctrl'
+//        });
+
+    });
+    
+}());
+    
