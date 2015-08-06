668359fb8917a1ca379b754f5dc849bc9a84972d
sjmp <samueljamespartridge@gmail.com>
2015-08-06 18:05:42 +0100

    Finished ng-show. Needs active class adding

--- app/app.routes.js
+++ app/app.routes.js
@@ -5,47 +5,47 @@ angular
     .config(function($routeProvider) {
         $routeProvider
 
-        //login
-        .when('/', {
-            templateUrl: 'app/components/login/loginView.html'
-            ,controller: 'loginController as ctrl'
-        })
-    
-        //register
-        .when('/register', {
-            templateUrl: 'app/components/register/registerView.html'
-            ,controller: 'registerController as ctrl'
-        })
+            //login
+            .when('/', {
+                templateUrl: 'app/components/login/loginView.html',
+                controller: 'loginController as ctrl'
+            })
 
-        //home
-        .when('/home', {
-            templateUrl: 'app/components/home/homeView.html'
-            ,controller: 'homeController as ctrl'
-        })
-        
-        //symptoms  
-        .when('/symptoms', {
-            templateUrl: 'app/components/symptoms/symptomsView.html'
-            ,controller: 'symptomsController as ctrl'
-        })
-        
-        //history
-        .when('/history', {
-            templateUrl: 'app/components/history/historyView.html'
-            ,controller: 'historyController as ctrl'
-        })
-        
-        //weight
-        .when('/weight', {
-            templateUrl: 'app/components/weight/weightView.html'
-            ,controller: 'weightController as ctrl'
-        })
-        
-        //dietician
-        .when('/food', {
-            templateUrl: 'app/components/food/foodView.html'
-            ,controller: 'foodController as ctrl'
-        });
+            //register
+            .when('/register', {
+                templateUrl: 'app/components/register/registerView.html',
+                controller: 'registerController as ctrl'
+            })
+
+            //home
+            .when('/home', {
+                templateUrl: 'app/components/home/homeView.html',
+                controller: 'homeController as ctrl'
+            })
+
+            //symptoms  
+            .when('/symptoms', {
+                templateUrl: 'app/components/symptoms/symptomsView.html',
+                controller: 'symptomsController as ctrl'
+            })
+
+            //history
+            .when('/history', {
+                templateUrl: 'app/components/history/historyView.html',
+                controller: 'historyController as ctrl'
+            })
+
+            //weight
+            .when('/weight', {
+                templateUrl: 'app/components/weight/weightView.html',
+                controller: 'weightController as ctrl'
+            })
+
+            //dietician
+            .when('/food', {
+                templateUrl: 'app/components/food/foodView.html',
+                controller: 'foodController as ctrl'
+            });
         
     });
     
--- app/components/register/registerController.js
+++ app/components/register/registerController.js
@@ -3,7 +3,17 @@
     angular
         .module('appetiteApp')   
         .controller('registerController', function() {
+            
             var self = this;
+            self.tab = 1;
+
+            this.setTab = function (tabId) {
+                self.tab = tabId;
+            };
+
+            this.isSet = function (tabId) {
+                return self.tab === tabId;
+            };
 
     });
 }());
