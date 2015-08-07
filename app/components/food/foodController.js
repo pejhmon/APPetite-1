bbda2e5ed48918c53f9c321b612a1a0b8f0ddac6
sjmp <samueljamespartridge@gmail.com>
2015-08-07 16:32:13 +0100

    Food modular aspects in there

--- app/components/food/foodController.js
+++ app/components/food/foodController.js
@@ -4,6 +4,17 @@
         .module('appetiteApp')   
         .controller('foodController', function() {
             var self = this;
-
+            self.tab = 1;
+        
+            this.setTab = function(tabId) {
+                self.tab = tabId;
+            };
+        
+            this.isSet = function(tabId) {
+                return self.tab === tabId;
+            };
+        
+            this.title = "Title";
+            this.body = "Body";
     });
 }());
