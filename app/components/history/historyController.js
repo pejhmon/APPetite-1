b92758504fee402e35b59d96befb78087ae3fe2e
sjmp <samueljamespartridge@gmail.com>
2015-08-08 15:51:57 +0100

    History view fleshed out with controller

--- app/components/history/historyController.js
+++ app/components/history/historyController.js
@@ -4,6 +4,16 @@
         .module('appetiteApp')   
         .controller('historyController', function() {
             var self = this;
+            self.tab = 'graph';
+            //includes 'dietician'
+
+            this.setTab = function (tabId) {
+                self.tab = tabId;
+            };
+
+            this.isSet = function (tabId) {
+                return self.tab === tabId;
+            };
 
     });
 }());
