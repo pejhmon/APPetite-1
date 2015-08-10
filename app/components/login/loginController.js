93a96250f80a9e2261d320aaf0454cca3c1ce465
sjmp <samueljamespartridge@gmail.com>
2015-08-10 19:20:40 +0100

    Registration all pages cleaned up, login partly done

--- app/components/login/loginController.js
+++ app/components/login/loginController.js
@@ -4,6 +4,9 @@
         .module('appetiteApp')   
         .controller('loginController', function() {
             var self = this;
-
+            
+            this.submit = function(){
+                console.log('User submitted with ', self.user);
+            };
     });
 }());
