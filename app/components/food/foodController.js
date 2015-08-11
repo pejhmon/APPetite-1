47f5e386f45b3fbfea5cd39cd5f085e91fde7a51
sjmp <samueljamespartridge@gmail.com>
2015-08-11 14:13:43 +0100

    Quick controller rework

--- app/components/food/foodController.js
+++ app/components/food/foodController.js
@@ -4,12 +4,24 @@
         .module('appetiteApp')   
         .controller('foodController', function() {
             var self = this;
+        
             self.tab = 'food';
                 //1 - food
                 //2 - new
                 //3 - frequent
                 //4 - meals
                 //5 - date
+            self.selected = [];
+        
+            self.items = [ 
+                'Chocolate Milk',
+                'Strawberry Milk',
+                'Strawberry Ice Cream',
+            ];
+            
+            self.title = self.tab;
+            self.body = "Body";
+            
         
             self.setTab = function(tabId) {
                 self.tab = tabId;
@@ -18,25 +30,13 @@
             self.isSet = function(tabId) {
                 return self.tab === tabId;
             };
-            
-            //Needs a switch
-            self.title = self.tab;
-            self.body = "Body";
-        
-        
-            self.items = [ 
-                'Chocolate Milk',
-                'Strawberry Milk',
-                'Strawberry Ice Cream',
-            ];
-        
+       
             self.itemsubmit = function(item){
                 self.selected.push(item);
                 console.log(item);
             }
         
-            self.selected = [
-            ];
+          
 
         
     });
