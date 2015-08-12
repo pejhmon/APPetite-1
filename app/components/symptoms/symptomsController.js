563fe6ca76e797f204671f2b1178fee1e4a0dadb
sjmp <samueljamespartridge@gmail.com>
2015-08-12 14:11:41 +0100

    Basic symptom working well, missing submit and symptoms not in list

--- app/components/symptoms/symptomsController.js
+++ app/components/symptoms/symptomsController.js
@@ -4,6 +4,38 @@
         .module('appetiteApp')   
         .controller('symptomsController', function() {
             var self = this;
+            self.selected = [];
+            self.tab;
+        
+            self.symptoms = [{ 
+                "Name": "Aching Fingers",
+              },
+              {
+                "Name": "Javascript Blues",
+              },
+              {
+                "Name": "Stinking Cold",
+              },
+              {
+                "Name": "Eyes Hurt",
+              },
+              {
+                "Name": "Tiredness",
+              },
+            ];
+        
+            self.selectSymptoms = function(toadd){
+                self.selected.push(toadd);
+                this.setTab(toadd.Name);
+            };
+        
+            this.setTab = function (tabId) {
+                self.tab = tabId;
+            };
+
+            this.isSet = function (tabId) {
+                return self.tab === tabId;
+            };
 
     });
 }());
